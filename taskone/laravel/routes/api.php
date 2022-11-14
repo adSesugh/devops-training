4af6d41c6e49aecfff1c7f4d01783c5fc14bd0ca
Sesugh Agbadu <sesughagbadu@yahoo.com>
2022-11-14 18:40:55 +0100

    refactoring to add more programming languages

---
+++ taskone/laravel/routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Http\Controllers\API\IndexController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::get('/root', [IndexController::class, 'index'])->name('api.index');
